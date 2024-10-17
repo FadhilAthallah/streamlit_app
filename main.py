143bf664e53813e4f0ae8457e803d3b2e08d0cac
FadhilAthallah <162326790+FadhilAthallah@users.noreply.github.com>
2024-10-17 10:44:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from prediction_user_input import prediction_user_input
+# from prediction_user_input import prediction_user_input
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -13,19 +13,19 @@ from prediction_csv_input import predict_from_csv
 def main():
     st.title("AI Behavior predict simulation")
 
-    # Select prediction method
-    prediction_method = st.sidebar.selectbox(
-        "Choose Prediction Method",
-        ("User Input", "CSV Upload")
-    )
+    # # Select prediction method
+    # prediction_method = st.sidebar.selectbox(
+    #     "Choose Prediction Method",
+    #     ("User Input", "CSV Upload")
+    # )
 
-    if prediction_method == "User Input":
-        st.write("### Predict from User Input")
-        prediction_user_input()
+    # if prediction_method == "User Input":
+    #     st.write("### Predict from User Input")
+    #     prediction_user_input()
 
-    else:
-        st.write("### Predict from CSV Upload")
-        predict_from_csv()
+    # else:
+    st.write("### Predict from CSV Upload")
+    predict_from_csv()
 
 if __name__ == "__main__":
     main()
