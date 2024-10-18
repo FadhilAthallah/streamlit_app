067cf1ab39eb2b962abd03dd7e71cd1d6bd1f3d0
FadhilAthallah <162326790+FadhilAthallah@users.noreply.github.com>
2024-10-18 10:17:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ from prediction_csv_input import predict_from_csv
 def main():
     st.title("AI Behavior predict simulation")
 
-    # # Select prediction method
-    # prediction_method = st.sidebar.selectbox(
-    #     "Choose Prediction Method",
-    #     ("User Input", "CSV Upload")
-    # )
+    # Select prediction method
+    prediction_method = st.sidebar.selectbox(
+        "Choose Prediction Method",
+        ("User Input", "CSV Upload")
+    )
 
-    # if prediction_method == "User Input":
-    #     st.write("### Predict from User Input")
-    #     prediction_user_input()
+    if prediction_method == "User Input":
+        st.write("### Predict from User Input")
+        prediction_user_input()
 
-    # else:
-    st.write("### Predict from CSV Upload")
-    predict_from_csv()
+    else:
+        st.write("### Predict from CSV Upload")
+        predict_from_csv()
 
 if __name__ == "__main__":
     main()
